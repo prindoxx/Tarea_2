c90e5e280ca80f71b8adfe8cead79f1a3d024d3b
prindoxx <pablortiz132@gmail.com>
2021-04-30 15:41:38 -0400

    inicio tarea

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "lista.h"
+#include "funciones.h"
+#include "map.h"
 
 int main()
 {
+
+    // Se crea la lista usando los datos del TDA lista implementado
+    list *lista_pokemon = list_create(lista_pokemon);
+
+    // Se abre el archivo de mundos csv en modo lectura "r"
+    FILE *fp = fopen ("pokemon_Archivo.csv", "r");
+
     printf("Hello world!\n");
     return 0;
 }
